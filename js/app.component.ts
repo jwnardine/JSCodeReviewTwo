2d984cea68adfa404538f178869bba19798dd718
Jon Nardine <jwnardine@gmail.com>
2016-03-25 08:43:51 -0700

    create initial app.component.ts, will need to make some changes

---
+++ js/app.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter } from 'angular2/core';
+import { Meal } from './meal.model';
+
+@Component({
+  selector: 'my-app',
+  directives: [KegListComponent],
+  template: `
+    <div class="container">
+      <p>app.component.ts</p>
+        <h1>Meal List</h1>
+        <meal-list
+          [mealList]="meals"
+          (onMealSelect)="mealWasSelected($event)">
+        </meal-list>
+      </div>
+  `
+})
+
+export class AppComponent {
+  public meals: Meal[];
+  constructor(){
+    this.meals = [
+      new Meal("Pizza", "Two slices of pizza, one Hawaiian and the other plain cheese", 400, 0),
+    ];
+  }
+  mealWasSelected(clickedMeal: Meal): void {
+    console.log("parent", clickedMeal);
+  }
+}
